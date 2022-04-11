fb524cce64031864042f62731aef58a9ec2fe445
Ogurchik-007 <nik.skripka.000@gmail.com>
2022-04-11 16:07:19 +0300

    created a header component

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import classes from './Header.module.scss';
 import logo from '../../assets/images/logo.png';
+import arrow from '../../assets/images/arrow.png';
+import favorites from '../../assets/images/favorites.png';
+import personalArea from '../../assets/images/personalArea.png';
+import basket from '../../assets/images/basket.png';
 
 const Header = (): JSX.Element => {
   return (
@@ -15,18 +19,28 @@ const Header = (): JSX.Element => {
             <img className={classes.logoImg} src={ logo } alt="логотип" />
           </section>
         <section className={classes.address}>
-          <img className={classes.addressImg} src="" alt="" />
-          <p></p>
-          <p></p>
+          <img className={classes.addressImg} src={arrow} alt="" />
+          <span className={classes.addressText}>
+            Санке-Петербур, ул Большая Конюшенная, д 19
+          </span>
         </section>
         <section className={classes.consumersCorner}>
-
+          <img className={classes.consumersCornerImg} src={favorites} alt="Избранное" />
+          <img className={classes.consumersCornerImg} src={personalArea} alt="Личгый кабинет" />
+          <img className={classes.consumersCornerImg} src={basket} alt="Корзина" />
         </section>
       </div>
 
       <div className= {classes.headerBottom}>
         <nav className={classes.nav}>
-
+          <a className={classes.navItem} href="header">Каталог Запчестей</a>
+          <a className={classes.navItem} href="header">Каталог ТО</a>
+          <a className={classes.navItem} href="header">Шины</a>
+          <a className={classes.navItem} href="header">Диски</a>
+          <a className={classes.navItem} href="header">Аккумуляторы</a>
+          <a className={classes.navItem} href="header">Автомасла</a>
+          <a className={classes.navItem} href="header">Автолампы</a>
+          <a className={classes.navItem} href="header">Аксессуары</a>
         </nav>
       </div>
     </header>
