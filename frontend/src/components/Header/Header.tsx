53cb837d6d4b0ed263483d2c23d3ef86abb20222
daniil_vasilenko <daniilvasilenko2@mail.ru>
2022-04-11 13:27:32 +0300

    feat(Header): refactor component, add .declare.ts

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,35 +2,36 @@ import React from 'react';
 import classes from './Header.module.scss';
 import logo from '../../assets/images/logo.png';
 
-const Header = () => {
-    return (
-      <div className={ classes.header }>
-        <div className={ classes.headerTop }>
-          <section className={ classes.basicInformation}>
-            <a className={ classes.basicInformationItem } href="header">Магазины</a>
-            <a className={ classes.basicInformationItem  } href="header">Акции</a>  
-            <a className={ classes.basicInformationItem  } href="header">Доставка и оплата</a>
+const Header = (): JSX.Element => {
+  return (
+    <header className={classes.header}>
+      <div className={classes.headerTop}>
+        <section className={classes.basicInformation}>
+          <a className={classes.basicInformationItem} href="header">Магазины</a>
+          <a className={classes.basicInformationItem} href="header">Акции</a>  
+          <a className={classes.basicInformationItem} href="header">Доставка и оплата</a>
+        </section>
+        <section className={classes.logo}>
+            <img className={classes.logoImg} src={ logo } alt="логотип" />
           </section>
-          <section className={ classes.logo }>
-             <img className={ classes.logoImg } src={ logo } alt="логотип" />
-            </section>
-          <section className={ classes.address }>
-            <img className={ classes.addressImg } src="" alt="" />
-            <p></p>
-            <p></p>
-          </section>
-          <section className={ classes.consumersCorner }>
+        <section className={classes.address}>
+          <img className={classes.addressImg} src="" alt="" />
+          <p></p>
+          <p></p>
+        </section>
+        <section className={classes.consumersCorner}>
 
-          </section>
-        </div>
-
-        <div className= { classes.headerBottom }>
-          <nav className={ classes.nav }>
-
-          </nav>
-        </div>
+        </section>
       </div>
-    );
-  }
+
+      <div className= {classes.headerBottom}>
+        <nav className={classes.nav}>
+
+        </nav>
+      </div>
+    </header>
+  );
+}
   
   export default Header;
+  
