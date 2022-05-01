4f90f313da1f912c2d69a5ff9c7dc2d348c5b179
daniil_vasilenko <daniilvasilenko2@mail.ru>
2022-05-01 19:53:08 +0300

    feat(eslint): upgrade

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,12 @@ import React from 'react';
 import Header from '../ui/Header';
 import Main from '../routes/Main';
 
+const lol = () => {
+  const a = 1;
+
+  return a;
+};
+
 const App: React.FC = () => (
   <div className="App">
     <Header />
